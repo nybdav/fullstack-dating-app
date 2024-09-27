ac387c94e36f7b98ad30cdb83bceaab5d908030d
David Nyberg <david.nyberg@solutionxperts.se>
2024-09-27 21:07:40 +0200

    Added DTOs for register and login

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0be928c1632445bc18f9777c0047e4d5c357fbe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3caa1d416d67739e0bf05d1a2ad5edb362f4d7bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
