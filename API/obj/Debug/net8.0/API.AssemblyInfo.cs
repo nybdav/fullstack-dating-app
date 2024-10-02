e12204992064b9463afee57257a33cb857e16775
David Nyberg <david.nyberg@solutionxperts.se>
2024-10-02 21:13:01 +0200

    added angular template forms to nav component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0fc67fcf9ed51bf6bdc8d174607122ae9150ad9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ce6901cc587f146d825dd6914b5d646e7d4b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
